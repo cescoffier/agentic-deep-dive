d6605d278ce707fcf9cf4cf99a73c6e0969a440d
mariofusco <mario.fusco@gmail.com>
2025-10-30 08:56:50 +0100

    fix a2a example configuration

--- 3-quarkus-ai-services/2-quarkus-ai-service-scopes/src/main/java/org/acme/AiServices.java
+++ 3-quarkus-ai-services/2-quarkus-ai-service-scopes/src/main/java/org/acme/AiServices.java
@@ -4,13 +4,18 @@ package org.acme;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 
 
 public class AiServices {
-
+    public static void main(String[] args) {
+        Quarkus.run();
+    }
     @RegisterAiService
     @RequestScoped
     interface ShortMemoryAssistant {
